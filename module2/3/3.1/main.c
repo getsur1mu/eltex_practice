49cda0b25e2e78981d050ffc7c8eccff523e7dd9
sur1mu <sur1mu@yahoo.com>
2026-04-15 14:12:02 +0700

    Project structure

---
+++ module2/3/3.1/main.c
@@ -0,0 +1,7 @@
+#include "source.h"
+
+int main(int argc, char *argv[])
+{
+    StartMascCalc(argc, argv);
+    return 0;
+}
---
+++ module2/3/3.1/source.h
@@ -0,0 +1,10 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <stdio.h>
+
+
+void StartMascCalc(int agrc, char* argv);
+
+
+#endif
